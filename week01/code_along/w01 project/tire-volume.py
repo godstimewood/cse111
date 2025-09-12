b2ce0947d0fb8a0a82bec0e36038f97de1d8c5a3
Godstime Wozuame <38727671+godstimewood@users.noreply.github.com>
2025-09-12 10:20:14 +0000

    The program calculates the volume of a tire and stores the values in a text file.

---
+++ week01/code_along/w01 project/tire-volume.py
@@ -0,0 +1,28 @@
+# I added a code to ask the user if want to buy the tire,
+# and stores their phone number in the volume.txt file if they say "yes".
+
+import math
+from datetime import datetime
+
+width = int(input("Enter the width of the tire in mm (ex 205:) "))
+aspect_ratio = int(input("Enter the aspect ratio of the tire (ex 60:) "))
+diameter = int(input("Enter the diameter of the wheel in inches (ex 15:) "))
+
+volume = (math.pi * (width ** 2) * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) /10000000000
+volume_rounded = round(volume, 2)
+
+print(f"The approximate volume is {volume_rounded} litres")
+
+current_date = datetime.now().strftime("%y-%m-%d")
+
+buy_tire = input("Would you like to buy tires with these diamensions? (yes/no): ").strip().lower()
+phone_number = ""
+
+if buy_tire == "yes":
+    phone_number = input("Please enter your phone number: ").strip()
+
+with open("volumes.txt", "at") as file:
+    if phone_number:
+        file.write(f"{current_date}, {width}, {aspect_ratio}, {diameter}, {volume_rounded}, {phone_number}\n")
+    else:
+        file.write(f"{current_date}, {width}, {aspect_ratio}, {diameter}, {volume_rounded}\n")
